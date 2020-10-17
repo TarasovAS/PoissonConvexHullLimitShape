9cfe4b806db19ac51092de493d226ef26be90d88
Alexander Tarasov <tarasovezik@gmail.com>
2020-10-17 14:10:26 +0300

    -

--- animator.py
+++ animator.py
@@ -3,14 +3,21 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 from math import cos, sin, sqrt, pi
 import math
-from random import random
+from celluloid import Camera
+from random import random, seed
 from geomalg import graham_scan, get_min_angle
+from datetime import datetime
 
 
 class Animator:
-    #TODO: вставить камеру из класса celluloid
+    def __init__(self, point_generator, generator_seed: int = 0) -> None:
 
-    def __init__(self, point_generator):
+        self.fig = plt.figure()
+        self.axis = self.fig.gca()
+        self.camera = Camera(self.fig)
+
+        self.generator_seed = generator_seed
+        seed(self.generator_seed)
         self.points = []
         self.generator = point_generator
         self.convex_hall = ConvexHall()
@@ -19,22 +26,44 @@ class Animator:
             self.points.append(self.generator.get_next())
         self.convex_hall.update_convex_hall(self.points)
 
+    def film_iterates(self, iterates_num):
+        for _ in range(iterates_num):
+            self.iterate()
+        return self.camera.animate()
+
     def iterate(self):
         points_to_generate = 1
         while self.generator.max_radius > self.convex_hall.max_radius:
             new_points = []
-            for i in range(points_to_generate):
-                next_point = self.generator.get_next
+            for _ in range(points_to_generate):
+                next_point = self.generator.get_next()
                 new_points.append(next_point)
                 self.points.append(next_point)
             points_to_generate *= 2
             self.convex_hall.update_convex_hall(new_points)
+            self.snap()
 
+        self.del_convex_hall_vertices()
+        self.snap()
+        self.convex_hall.update_convex_hall(self.points)
 
-    def add_point(self):
-        self.points.append(self.generator.get_next)
+    def del_convex_hall_vertices(self):
+        while self.convex_hall.vertices:
+            vertex = self.convex_hall.vertices.pop()
+            self.points.remove(vertex)
 
+    def snap(self):
+        self.axis.clear()
 
+        for point in self.points:
+            self.axis.scatter(point[0], point[1])
+
+        if self.convex_hall.vertices:
+            points = self.convex_hall.vertices[:] + self.convex_hall.vertices[0]
+            for point1, point2 in zip(points[:-1], points[1:]):
+                self.axis.plot([point1[0], point2[0]], [point1[1], point2[1]])
+
+        self.camera.snap()
 
 
 class PointGenerator:
@@ -52,7 +81,7 @@ class PointGenerator:
 
 class ConvexHall:
 
-    def __init__(self, points: list = []):
+    def __init__(self, points: list):
         self.vertices = points
         self.max_radius = np.inf
         if self.vertices:
@@ -61,9 +90,21 @@ class ConvexHall:
     def update_convex_hall(self, points: list):
         self.vertices = graham_scan(self.vertices + points)
 
-        min_vector_len = min([sqrt(x**2+y**2) for x,y in self.vertices])
+        min_vector_len = min([sqrt(x**2 + y**2) for x, y in self.vertices])
         min_angle = get_min_angle(self.vertices)
         self.max_radius = min_vector_len * cos(min_angle / 2)
 
 
-def main():
+def main(frames, generator_seed=0):
+    print(0)
+    generator = PointGenerator
+    print(1)
+    animator = Animator(generator, generator_seed)
+    print(2)
+    animation = animator.film_iterates(frames)
+    print(3)
+    animation.save(f'convex_hall_{frames}_frames_{generator_seed}_{datetime.today()}.gif', writer = 'imagemagick')
+
+print(-2)
+#main(frames = 1, generator_seed = 0)
+print(-1)
---
+++ geom_algorithms.py
@@ -0,0 +1,43 @@
+from math import atan2 as arctg
+from math import pi
+
+
+def signed_area_of_triangle(a, b, c):
+    """ compute signed area of triangle """
+    return (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])
+
+
+def graham_scan(points):
+    """ from list of points select vertices of their convex hall """
+    number_of_points = len(points)  # число точек
+    p = list(range(number_of_points))  # список номеров точек
+    for i in range(1, number_of_points):
+        if points[p[i]][0] < points[p[0]][0]:  # если p[i]-ая точка лежит левее p[0]-ой точки
+            p[i], p[0] = p[0], p[i]  # меняем местами номера этих точек
+    for i in range(2, number_of_points):  # сортировка вставкой
+        j = i
+        while j > 1 and (signed_area_of_triangle(points[p[0]], points[p[j - 1]], points[p[j]]) < 0):
+            p[j], p[j - 1] = p[j - 1], p[j]
+            j -= 1
+    convex_hall = [p[0], p[1]]  # создаем стек
+    for i in range(2, number_of_points):
+        while signed_area_of_triangle(points[convex_hall[-2]], points[convex_hall[-1]], points[p[i]]) < 0:
+            del convex_hall[-1]  # pop(convex_hall)
+        convex_hall.append(p[i])  # push(convex_hall,p[i])
+    return convex_hall
+
+
+def get_min_angle(points: list) -> float:
+    points = points[:] + [points[0]]
+    angles = [get_angle(p1, p2) for p1, p2 in zip(points[:-1], points[1:])]
+    return min(angles)
+
+
+def get_angle(p2, p1):
+    angle = arctg(p2[0], p2[1]) - arctg(p1[0], p1[1])
+    if angle < 0:
+        angle += 2*pi
+    assert 0 <= angle <= 2*pi
+    return angle
+
+
